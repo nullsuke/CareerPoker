c081bd2d1de7fc78b47ac8e1fade5c9c76c6291f
Nullsuke <nullsuke@gmail.com>
2021-02-09 22:55:28 +0900

    メッセージを描画

---
+++ Script/TextViewer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextViewer : MonoBehaviour
+{
+    private RectTransform rect;
+    private Text text;
+
+    public void Pop(string msg, Vector2 pos, Color textColor)
+    {
+        Render(msg, pos, textColor);
+        Destroy(gameObject, 1f);
+    }
+
+    public void Render(string msg, Vector2 pos, Color textColor)
+    {
+        rect.anchoredPosition = pos;
+        text.text = msg;
+        text.color = textColor;
+    }
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        text = GetComponent<Text>();
+    }
+}

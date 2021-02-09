85e840eef191364e68c7bbb8ecfd206e472e2846
Nullsuke <nullsuke@gmail.com>
2021-02-09 22:44:43 +0900

    場のカード、使用されたカード、プレイヤーの人数などを保持

---
+++ Script/GameData.cs
@@ -0,0 +1,24 @@
+﻿public class GameData
+{
+    private readonly ulong bitFieldCard;
+    private readonly ulong bitUsedCard;
+    private readonly int playingNumber;
+    private readonly int playerNumber;
+    private readonly bool isRevolutionalizing;
+
+    public GameData(ulong bitFieldCard, ulong bitUsedCard,
+         int playingNumber, int playerNumber, bool isRevolutionalizing)
+    {
+        this.playingNumber = playingNumber;
+        this.playerNumber = playerNumber;
+        this.bitFieldCard = bitFieldCard;
+        this.bitUsedCard = bitUsedCard;
+        this.isRevolutionalizing = isRevolutionalizing;
+    }
+
+    public ulong BitFieldCard { get => bitFieldCard; }
+    public ulong BitUsedCard { get => bitUsedCard; }
+    public int PlayingNumber { get => playingNumber; }
+    public int PlayerNumber { get => playerNumber; }
+    public bool IsRevolutionalizing { get => isRevolutionalizing; }
+}

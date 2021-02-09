5e26f840ade5e3c93bfb17644beb558875ef04a1
Nullsuke <nullsuke@gmail.com>
2021-02-09 22:32:56 +0900

    コンピュータの手札の管理・描画

---
+++ Script/Computer/ComputerController.cs
@@ -0,0 +1,39 @@
+﻿public class ComputerController : PlayerController
+{
+    private PlayerCardViewer viewer;
+
+    public override void OnTurn(ulong fieldCard, ulong usedCard, int playingMember,
+        int playerMember, bool isRevolutionalizing)
+    {
+        base.OnTurn(fieldCard, usedCard, playingMember,
+            playerMember, isRevolutionalizing);
+
+        BitSelectedHand = logic.SelectBitHand(ID, gameData);
+        
+        if (BitSelectedHand != 0) logic.DrawCard(ID, BitSelectedHand);
+
+        OnEndedTurn();
+    }
+
+    public override void Render()
+    {
+        viewer.Render(ToCardIDList(BitPlayerCard));
+    }
+
+    public override void Clear()
+    {
+        base.Clear();
+        Render();
+    }
+
+    public override void Open()
+    {
+        viewer.Open();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        viewer = GetComponent<PlayerCardViewer>();
+    }
+}

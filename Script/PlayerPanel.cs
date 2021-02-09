12a65065ad363eacd3617636db2d80827bd850d7
Nullsuke <nullsuke@gmail.com>
2021-02-09 22:53:58 +0900

    各プレイヤーのパネルの色を管理

---
+++ Script/PlayerPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPanel : MonoBehaviour
+{
+    [SerializeField] Color panelColor = default;
+    private Image image;
+    private Color defaultColor;
+
+    public void ChangeColor()
+    {
+        image.color = panelColor;
+    }
+
+    public void RestoreColor()
+    {
+        image.color = defaultColor;
+    }
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        defaultColor = image.color;
+    }
+}

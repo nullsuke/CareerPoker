38216b0304b1417df3a4838478c408a6d68c73b9
Nullsuke <nullsuke@gmail.com>
2021-02-09 22:33:24 +0900

    手札の描画

---
+++ Script/Computer/PlayerCardViewer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCardViewer : MonoBehaviour
+{
+    [SerializeField] Card cardPrefab = default;
+    [SerializeField] bool isCardFace = default;
+    protected List<Card> cards;
+
+    public virtual void Render(List<int> idList)
+    {
+        Clear();
+
+        idList.ForEach(i => cards.Add(CreateCard(i)));
+    }
+
+    public void Clear()
+    {
+        foreach (Transform c in transform)
+        {
+            Destroy(c.gameObject);
+        }
+
+        cards.Clear();
+    }
+
+    public void Open()
+    {
+        cards.ForEach(c => c.IsFace = true);
+    }
+
+    private void Awake()
+    {
+        cards = new List<Card>();
+    }
+
+    protected virtual Card CreateCard(int i)
+    {
+        var card = Instantiate(cardPrefab, transform, false);
+        card.ID = i;
+        card.IsFace = isCardFace;
+
+        return card;
+    }
+}
